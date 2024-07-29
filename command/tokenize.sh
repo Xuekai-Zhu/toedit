16099f0b41485824341f820afae4be5c9a22e091
xuekai <xuekaizhu0@gmail.com>
2024-07-29 17:44:15 +0800

    update

--- command/tokenize.sh
+++ command/tokenize.sh
@@ -1,4 +1,4 @@
-python OLMo/scripts/prepare_memmap_dataset.py data/bio/biomed+orca/*.json.gz \
-    -o data/bio/biomed+orca_tokenized \
+python OLMo/scripts/prepare_memmap_dataset.py data/math/akjindal53244/Arithmo-Data—1B/*.json.gz \
+    -o data/math/akjindal53244/Arithmo-Data—1B_tokenized \
     --tokenizer OLMo/olmo_data/tokenizers/allenai_gpt-neox-olmo-dolma-v1_5.json \
     --workers 4
