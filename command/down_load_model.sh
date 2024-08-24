6d4d6c65ee695703f4907c87f968997233b21e7f
xuekai <xuekaizhu0@gmail.com>
2024-08-24 22:25:35 +0800

    update

--- command/down_load_model.sh
+++ command/down_load_model.sh
@@ -1,8 +1,8 @@
 
 export HF_ENDPOINT=https://hf-mirror.com
 
-huggingface-cli download Qwen/Qwen2-1.5B \
+huggingface-cli download HuggingFaceTB/fineweb-edu-classifier \
   --repo-type model \
   --resume-download \
-  --local-dir pre_trained_model/Qwen/Qwen2-1.5B \
+  --local-dir pre_trained_model/HuggingFaceTB/fineweb-edu-classifier \
   --local-dir-use-symlinks False
