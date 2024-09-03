8257afcd6e3c30ce687b0fa8b8efd4ab50d90e7f
xuekai <xuekaizhu0@gmail.com>
2024-09-03 20:47:01 +0800

    update

--- command/tokenize.sh
+++ command/tokenize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python OLMo/scripts/prepare_memmap_dataset.py  data/bio/biomed_8_Meta-Llama3-8B-Instruct_gt_0.99_resmapling/*.json.gz \
-    -o data/bio/biomed_8_Meta-Llama3-8B-Instruct_gt_0.99_resmapling_tokenized \
+python OLMo/scripts/prepare_memmap_dataset.py  data/bio/instruction_biomed—1B-up_revise/*.json.gz \
+    -o data/bio/instruction_biomed—1B-up_revise_tokenized \
     --tokenizer OLMo/tokenizers/allenai_gpt-neox-olmo-dolma-v1_5.json \
     --workers 8
