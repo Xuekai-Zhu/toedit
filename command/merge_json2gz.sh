9ecd4d6e0802916d0b940b8197fa4bab64a39576
xuekai <xuekaizhu0@gmail.com>
2024-09-03 15:31:37 +0800

    update

--- command/merge_json2gz.sh
+++ command/merge_json2gz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INPUT_DIR="probability/biomed_8_Meta-Llama3-8B-Instruct_gt_0.99"
-OUTPUT_DIR="data/bio/biomed_8_Meta-Llama3-8B-Instruct_gt_0.99_resmapling"
+INPUT_DIR="probability/instruction_biomed—1B-low_drop_up_revise"
+OUTPUT_DIR="data/bio/instruction_biomed—1B-low_drop_up_revise"
 CHUNK_SIZE=1000000  
 
 python data_process/merge_jsonl2gz.py --input_dir "$INPUT_DIR" --output_dir "$OUTPUT_DIR" --chunk_size $CHUNK_SIZE
