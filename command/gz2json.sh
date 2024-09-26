8a9e5425a6aa3fb05d7e31e10bb07f17090c52ba
xuekai zhu <xuekaizhu0@gmail.com>
2024-09-26 03:10:28 +0000

    update

--- command/gz2json.sh
+++ command/gz2json.sh
@@ -1,3 +1,3 @@
-for file in data/bio/biomed_revised+orca/*.json.gz; do
+for file in /home/zhuxuekai/scratch2_nlp/scaling_down_data/data/bio/biomed+orca-1B-json/biomed+orca-1B/*.json.gz; do
     gzip -d "$file"
 done
