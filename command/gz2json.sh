df58e9897dc55782f105e8eec9b86baf6068435f
xuekaizhu_linlab <xuekaizhu0@gmail.com>
2024-11-19 14:04:41 +0800

    update

--- command/gz2json.sh
+++ command/gz2json.sh
@@ -1,3 +1,3 @@
-for file in data/less-data-llama-revised/oasst1-json-gz/*.json.gz; do
+for file in data/sft/code-data-revised/Magicoder-OSS-Instruct-75K-json-gz/*.json.gz; do
     gzip -d "$file"
 done
--- command/merge_json2gz.sh
+++ command/merge_json2gz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INPUT_DIR="/home/xkzhu/scaling_down_data/probability/sft/less-data/oasst1-json-gz"
-OUTPUT_DIR="data/less-data-llama-revised/oasst1-json-gz"
-CHUNK_SIZE=100000  
+INPUT_DIR="/home/xkzhu/scaling_down_data/probability/sft/code-data/Magicoder-OSS-Instruct-75K-json-gz"
+OUTPUT_DIR="data/sft/code-data-revised/Magicoder-OSS-Instruct-75K-json-gz"
+CHUNK_SIZE=1000000  
 
 python data_process/merge_jsonl2gz.py --input_dir "$INPUT_DIR" --output_dir "$OUTPUT_DIR" --chunk_size $CHUNK_SIZE
